4a109446f91843f7f15aa50e66dd7c6a7af713bd
Mariusz Jarosz <yahuto@hotmail.com>
2018-02-14 09:56:20 +0100

    dodanie lekcji 21

---
+++ MistrzowieWynajmu/MistrzowieWynajmu/Models/Address.cs
@@ -0,0 +1,9 @@
+﻿namespace MistrzowieWynajmu.Models
+{
+    public class Address
+    {
+        public int AddressId { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+    }
+}
---
+++ MistrzowieWynajmu/MistrzowieWynajmu/Models/Property.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MistrzowieWynajmu.Models
+{
+    public class Property
+    {
+        public int Id { get; set; }
+        public PropertyType Type { get; set; }
+        public string Description { get; set; }
+        public int Rooms { get; set; }
+        public int Area { get; set; }
+        public bool Washer { get; set; }
+        public bool Refrigerator { get; set; }
+        public bool Iron { get; set; }
+
+        public virtual int AddressIs { get; set; }
+        public virtual Address Address { get; set; }
+
+        public virtual int OwnerId { get; set; }
+        public virtual Owner Owner { get; set; }
+
+
+    }
+
+    public enum PropertyType
+    {
+        House = 0,
+        Flat = 1
+    }
+}
